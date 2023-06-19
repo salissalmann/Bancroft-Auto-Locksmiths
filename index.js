302631fe41a585dfac65829aa1dca639e712e6f0
salissalmann <i200489@nu.edu.pk>
2023-06-20 04:53:20 +0500

    Cors fix

--- index.js
+++ index.js
@@ -11,11 +11,14 @@ const FetchUser = require('./Middleware')
 const DBstring = process.env.MONGO_URL;
 
 app.use(cors({
-    origin: 'http://localhost:3000',
-    origin: 'http://127.0.0.1:3000',
+  origin: [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'https://bancroftautolocksmiths.onrender.com/'
+  ],
+  optionsSuccessStatus: 200
+}));
 
-    optionsSuccessStatus: 200
-  }));
 mongoose.set('strictQuery',true)
 const ConnectToMongo = async () => {
   try {
